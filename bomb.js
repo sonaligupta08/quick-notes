0a0c6a834729f4ca5c53849efafc08c3701bd8f4
sonali <misssonali2604@gmail.com>
2025-02-20 13:14:30 +0530

    Initial commit

--- bomb.js
+++
@@ -1,5 +0,0 @@
-function bomb() {
-
-    let image = document.querySelector(".container img");
-    image.src = "https://media.istockphoto.com/id/470309868/photo/explosion-nuclear-bomb-in-ocean.jpg?s=612x612&w=0&k=20&c=gA9WPuNBPkpox4bNZcPBit-A0HkxmTpYsHKYlNiX8z8="
-}
